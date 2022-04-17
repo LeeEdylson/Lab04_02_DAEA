6f3f14a40a78ff1e7e8927c1743704f079256f6c
leeedylson19 <lee.edylson19@gmail.com>
2022-04-16 23:18:38 -0500

    first commit

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp_lab04
+{
+    class Person
+    {
+        public string PersonId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FullName { get; set; }
+    }
+}
